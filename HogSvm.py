548099a3f0bb60ea3c5e5ab76b0777148a2eeed7
unknown <zhengyuan.xie@mail.utoronto.ca>
2019-12-01 14:45:06 -0500

    Add comments to HogSvm.py

--- HogSvm.py
+++ HogSvm.py
@@ -23,13 +23,16 @@ def computeHOGs(img_lst, wsize=(128, 64)):
     hog = cv2.HOGDescriptor()
     print(len(img_lst))
     for img in img_lst:
+        # img = np.power(img/float(np.max(img)), 1/1.5)
         if img.shape[1] > wsize[1] and img.shape[0] > wsize[0]:
             H, W = img.shape[:2]
             roi = img[(H - wsize[0]) // 2: (H - wsize[0]) // 2 + wsize[0], (W - wsize[1]) // 2: (W - wsize[1]) // 2 + wsize[1]]
             gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+            gray = np.power(img/float(np.max(gray)), 1.5)
             gradientList.append(hog.compute(gray))
         else:
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            gray = np.power(gray, 1/1.5).astype(np.uint8)
             gradientList.append(hog.compute(gray))
     return gradientList
     
@@ -60,9 +63,10 @@ def findHardExamples(svm, negList):
     hog = cv2.HOGDescriptor()
     detector = getSvmDetector(svm)
     hog.setSVMDetector(detector)
+    # let hog detect each negative example, hog is expected to give no rectangles when given negative examples
     for neg in negList:
         rects, weights = hog.detectMultiScale(neg, winStride=(4, 4),padding=(8, 8), scale=1.05)
-        # crop out each part which misleads the model and resize them to be a new negative example
+        # crop out each rectangle part which misleads the model and resize them to be a new negative example
         for (x,y,H,W) in rects:
             hardExample = neg[y:y+W, x:x+H]
             hardExamples.append(cv2.resize(hardExample,(64, 128)))
@@ -77,33 +81,38 @@ if __name__ == "__main__":
     labels = []
 
     # for now, pos-50 and neg-110 have the best performance
-    positiveList = loadImages(posPath, num = 100)
-    negativeList = loadImages(negPath, num = 200)
+    positiveList = loadImages(posPath, num = 200)
+    negativeList = loadImages(negPath, num = 400)
     # sample_neg(full_neg_lst, negativeList, [128, 64])
 
     print("The number of positive examples:", len(positiveList))
     print("The number of negative examples:", len(negativeList))
 
+    # gradienList stores gradients of both positive and negative examples
+    # labels stores 1 for positive examples and -1 for engative examples
     gradientList = computeHOGs(positiveList) + computeHOGs(negativeList)
     labels = [1 for _ in positiveList] + [-1 for _ in negativeList]
     
     # train svm on positive and negative examples
+    # training svm needs images' gradients and their corresponding labels
     svm = createSvm()
     svm.train(np.array(gradientList), cv2.ml.ROW_SAMPLE, np.array(labels))
     print("Finished first round of training")
     
-    # train svm on negative examples which cause poor performance
+    # search for negative examples which cause poor performance
+    # negative examples which cause model to give rectangles are hard
     hardNegList = findHardExamples(svm, negativeList)
     print("Size of hard_negativeList:", len(hardNegList))
     gradientList += computeHOGs(hardNegList)
 
+    # extend labels by adding -1 for new negative examples
     for i in range(len(hardNegList)):
         labels.append(-1)
 
     svm.train(np.array(gradientList), cv2.ml.ROW_SAMPLE, np.array(labels))
     print("Finished second round of training")
     
-    # comebine hog with our detected  save svm+hog model
+    # comebine hog with our trained svm detector, then save svm+hog model
     detector = getSvmDetector(svm)
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector(detector)
